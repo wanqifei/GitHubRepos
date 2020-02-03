5ba60fa27824c0454357dbd31ab0379a10dffaaa
Figo666 <wan.qifei@outlook.com>
2020-02-03 22:04:09 +0800

    Add mssql module to nodejs but not connect to sql server successfully

--- Nodejs/startup.js
+++ Nodejs/startup.js
@@ -1,1 +1,25 @@
 //Node.js file
+const sql=require('mssql');
+const connectconfig = {
+    user: 'sa',
+    password: 'P@ssword',
+    server: '(LocalDB)\\MSSQLLocalDB',
+    database: 'Historian'
+};
+//UID=sa;PWD=P@ssword;
+const connectstr = 'Driver=msnodesqlv8;Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Database=Historian;';
+
+(async function () {
+    try {
+        let pool = await sql.connect(connectstr);
+        let result1 = await pool.request().query('SELECT * FROM dbo.TimeLog;');
+        console.dir(result1);
+        await pool.close()
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+sql.on('error', err => {
+    console.log('连接失败!');
+});
